c610f7f8f5de6fc15e96b4741bc87950f284f243
Nitin0077 <79240252+Nitin0077@users.noreply.github.com>
2026-05-14 21:08:30 +0530

    Initial code Implement Live Database Server

--- backend/server.js
+++ backend/server.js
@@ -11,11 +11,15 @@ app.use(express.json());
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
+
 
 db.connect((err) => {
   if (err) {
@@ -126,6 +130,6 @@ app.delete("/notes/:id", (req, res) => {
 });
 
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
 });
